2888f751d80d22eb54d5ec286ff5bb58c47ac025
shrutijpg <shrutibh1001@gmail.com>
2025-04-22 16:08:46 +0530

    Time: 3 ms (71.44%) | Memory: 15.7 MB (50.50%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<string> generateParenthesis(int n) {
+        vector<string> v;
+        solve(n,n,"",v);
+        return v;
+
+    }
+    void solve(int open,int close,string op,vector<string>&v){
+        if(open == 0 && close ==0){
+            v.push_back(op);
+            return;
+        }
+        if(open>0){
+
+            solve(open-1,close,op + '(',v);
+        }
+        if(close>open){
+            
+            solve(open,close-1,op + ')',v);
+        }
+
+    }
+};
