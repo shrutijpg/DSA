8810c201f9a04aec60e29f9a285302dc6019ab8a
shrutijpg <shrutibh1001@gmail.com>
2025-06-25 14:19:49 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (72.30%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if( n==0 || n==1) return 1;
+
+        int prev1 , prev2, curr;
+        prev1=1;
+        prev2=1;
+
+        for(int i=2;i<=n;i++){
+            curr = prev2 + prev1;
+            prev2 = prev1;
+            prev1 = curr; 
+
+        }
+        return curr;
+    }
+};
