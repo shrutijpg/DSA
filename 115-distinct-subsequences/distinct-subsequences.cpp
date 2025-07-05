c327b2332c7514aabe0b338b82fa85ffe63603d3
shrutijpg <shrutibh1001@gmail.com>
2025-07-05 11:32:10 +0530

    Time: 31 ms (54.44%) | Memory: 44.2 MB (32.09%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int mod = 1e9 + 7;
     int func(int i, int j,string&s, string &t,vector<vector<int>>&dp){
-        if(j<0) return 1;
-        if(i<0) return 0;
+        if(j==0) return 1;
+        if(i==0) return 0;
 
         if(dp[i][j]!= -1) return dp[i][j];
 
-        if(s[i] == t[j]){
+        if(s[i-1] == t[j-1]){
             return dp[i][j]=(func(i-1,j-1,s,t,dp) + func(i-1,j,s,t,dp))%mod;
             
         }
@@ -19,9 +19,28 @@ public:
         int n = s.size();
         int m = t.size();
 
-        vector<vector<int>>dp(n,vector<int>(m,-1));
+        vector<vector<double>>dp(n+1,vector<double>(m+1,0));
 
-        return func(n-1,m-1,s,t,dp);
+        for(int i=0;i<=n;i++){
+            dp[i][0] = 1;
+        }
+       // for(int j=1;j<=m;j++){
+      //      dp[0][j] = 0;
+       // }
+
+        for(int i = 1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1] == t[j-1]){
+                    dp[i][j]=(dp[i-1][j-1]+ dp[i-1][j]);
+                }
+                else{
+                    dp[i][j]=dp[i-1][j];
+                }
+                
+            }
+        }
+
+        return (int)dp[n][m];
         
     }
 };
