abb838fbc4086883f8f3d9f941b7cb4bc4927cfe
shrutijpg <shrutibh1001@gmail.com>
2025-07-06 23:07:22 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (81.55%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,24 +1,28 @@
 class Solution {
 public:
-
-    int func(int ind,int buy,vector<int>& prices,int n,vector<vector<int>> &dp){
-        long profit;
-        if(ind>=n) return 0;
-
-        if(dp[ind][buy] != -1) return dp[ind][buy];
-
-        if(buy==1){
-            profit = max((-prices[ind]+func(ind+1,0,prices,n,dp)),(0+func(ind+1,1,prices,n,dp)));
-        }
-        else{
-            profit = max((prices[ind]+func(ind+2,1,prices,n,dp)),(0+func(ind+1,0,prices,n,dp)));
-        }
-        return dp[ind][buy] = profit;
-    }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        vector<vector<int>> dp(n,vector<int>(2,-1));
-        return func(0,1,prices,n,dp);
+        if (n <= 1) return 0;
         
+        // Initialize variables to track states
+        int ahead2_buy = 0;  // dp[i+2][1]
+        int ahead1_buy = 0;  // dp[i+1][1]
+        int ahead1_notBuy = 0;  // dp[i+1][0]
+        int curr_buy, curr_notBuy;
+        
+        for (int ind = n-1; ind >= 0; ind--) {
+            // Can buy
+            curr_buy = max(-prices[ind] + ahead1_notBuy, ahead1_buy);
+            
+            // Can sell (with cooldown)
+            curr_notBuy = max(prices[ind] + ahead2_buy, ahead1_notBuy);
+            
+            // Update for next iteration
+            ahead2_buy = ahead1_buy;
+            ahead1_buy = curr_buy;
+            ahead1_notBuy = curr_notBuy;
+        }
+        
+        return ahead1_buy;
     }
 };
