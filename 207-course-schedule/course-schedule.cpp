74fe53a23a5cb89109ca4bcb8f0695a3937b454c
shrutijpg <shrutibh1001@gmail.com>
2025-07-08 22:20:12 +0530

    Time: 3 ms (79.70%) | Memory: 19.4 MB (63.69%) - LeetSync

---
+++ 207-course-schedule/course-schedule.cpp
@@ -0,0 +1,50 @@
+class Solution {
+public:
+
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        //forming the graph
+        vector<vector<int>>adj(numCourses);
+        for (auto &pre : prerequisites) {
+            int u = pre[1]; 
+            int v = pre[0];
+            adj[u].push_back(v);
+        }
+
+        // Step 2: Compute indegrees
+        vector<int> indegree(numCourses, 0);
+        for (int i = 0; i < numCourses; i++) {
+            for (auto it : adj[i]) {
+                indegree[it]++;
+            }
+        }
+
+        // Step 3: Push all nodes with indegree 0 into the queue
+        queue<int> q;
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] == 0) {
+                q.push(i);
+            }
+        }
+
+        // Step 4: Process the queue
+        vector<int> topo;
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+
+            for (auto it : adj[node]) {
+                indegree[it]--;
+                if (indegree[it] == 0) {
+                    q.push(it);
+                }
+            }
+        }
+
+        
+
+        if(topo.size()== numCourses)return true;
+        return false;
+
+    }
+};
