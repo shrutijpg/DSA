9b546ec7815bcb45d8085db9260f70c2893f1d25
shrutijpg <shrutibh1001@gmail.com>
2025-07-18 17:24:57 +0530

    Time: 0 ms (100.00%) | Memory: 20.3 MB (84.59%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-    int func(int ind, vector<int>& nums, vector<int>& dp) {
+    int jump(vector<int>& nums) {
+        int jumps = 0;
+        int l = 0, r = 0;
         int n = nums.size();
-        if (ind >= n - 1) return 0; // No more jumps needed if we are at or beyond last index
-        if (dp[ind] != -1) return dp[ind];
 
-        int minJumps = INT_MAX;
-        int maxStep = nums[ind];
-        for (int i = 1; i <= maxStep; i++) {
-            int next = func(ind + i, nums, dp);
-            if (next != INT_MAX) // avoid overflow
-                minJumps = min(minJumps, 1 + next);
+        while (r < n - 1) {
+            int farthest = 0;
+            for (int i = l; i <= r; i++) {
+                farthest = max(farthest, i + nums[i]);
+            }
+            l = r + 1;
+            r = farthest;
+            jumps++;
         }
 
-        return dp[ind] = minJumps;
-    }
-
-    int jump(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> dp(n, -1);
-        return func(0, nums, dp);
+        return jumps;
     }
 };
