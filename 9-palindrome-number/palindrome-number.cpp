a241da9b9fd0b4b0f3b5b09128a2a999b49fa837
shrutijpg <shrutibh1001@gmail.com>
2025-07-19 21:22:58 +0530

    Time: 4 ms (33.40%) | Memory: 8.5 MB (92.14%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -4,13 +4,13 @@ public:
         long revNum=0;
         int dup=x;
 
-        while(x>0){
-            int ld = x % 10;
+        while(dup>0){
+            int ld = dup % 10;
 
             revNum = (revNum * 10) + ld;
-            x=x/10;
+            dup=dup/10;
         }
-        if(dup==revNum) return true;
+        if(revNum==x) return true;
         else return false;
     }
 };
