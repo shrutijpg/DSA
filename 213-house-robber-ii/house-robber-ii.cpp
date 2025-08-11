09f36e668805df95547673a3d60a56867186fba6
shrutijpg <shrutibh1001@gmail.com>
2025-08-11 16:50:23 +0530

    Time: 0 ms (100.00%) | Memory: 11 MB (8.66%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.cpp
+++ 213-house-robber-ii/house-robber-ii.cpp
@@ -1,39 +1,34 @@
 class Solution {
 public:
-   
-    int rob1(vector<int>& nums) {
+    int rob1(vector<int>& nums){
         int n = nums.size();
-        int prev = nums[0];
-        int prev2 = 0;
+        if(n==0) return 0;
+        if(n==1) return nums[0];
+        vector<int>  dp (n+1,-1);
 
-        for(int i=1;i<n;i++){
-            int take = nums[i];
-            if(i>1){
-                take += prev2;
-            }
-            int non_take = 0 + prev;
+        dp[0] = nums[0];
+        dp[1] = max(nums[1],nums[0]);
 
-            int curr = max(take,non_take);
+        for(int i =2;i<n;i++){
+            int take = nums[i] + dp[i-2];
+            int not_take = dp[i-1];
 
-            prev2 = prev;
-            prev = curr;
-
+            dp[i] = max(take,not_take);
 
         }
-        return prev;
-
+        return dp[n-1];
     }
-
     int rob(vector<int>& nums) {
-        
         int n = nums.size();
         if(n==1) return nums[0];
-        vector<int> temp1,temp2;
+        vector<int> temp1 ,temp2;
         for(int i=0;i<n;i++){
             if(i != 0) temp1.push_back(nums[i]);
-            if( i != n-1) temp2.push_back(nums[i]);
+            if(i != n-1)  temp2.push_back(nums[i]);
+
 
         }
         return max(rob1(temp1),rob1(temp2));
+        
     }
 };
