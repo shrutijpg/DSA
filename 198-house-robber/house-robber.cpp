8b12cb6aa23580c26f5b181bcadda6c69506eedd
shrutijpg <shrutibh1001@gmail.com>
2025-08-11 15:14:15 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (22.51%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
-   
+    int func(int ind,vector<int>& nums,vector<int>&dp){
+        if(ind ==0) return nums[ind];
+        if(ind<0) return 0;
+        if (dp[ind] != -1)  return dp[ind];
+
+        int pick = func(ind-2,nums,dp)+ nums[ind];
+
+        int not_pick = func(ind-1,nums,dp);
+
+        return dp[ind]= max(pick,not_pick);
+    }
     int rob(vector<int>& nums) {
         int n = nums.size();
-        int prev = nums[0];
-        int prev2 = 0;
 
-        for(int i=1;i<n;i++){
-            int take = nums[i];
-            if(i>1){
-                take += prev2;
-            }
-            int non_take = 0 + prev;
-
-            int curr = max(take,non_take);
-
-            prev2 = prev;
-            prev = curr;
-
-
-        }
-        return prev;
-
+        vector<int> dp(n+1,-1);
+        return func(n-1,nums,dp);
+        
     }
 };
