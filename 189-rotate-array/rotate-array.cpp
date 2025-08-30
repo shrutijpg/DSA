09c8bfb3cb86fce7121aa2bdce50b05a85013f46
shrutijpg <shrutibh1001@gmail.com>
2025-08-30 14:48:26 +0530

    Time: 0 ms (100.00%) | Memory: 29.5 MB (61.08%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -2,22 +2,11 @@ class Solution {
 public:
     void rotate(vector<int>& nums, int k) {
         int n = nums.size();
-        k = k % n;  // handle if k > n
+        k = k % n;  
 
-        // store last k elements in temp
-        vector<int> temp(k);
-        for(int i=0; i<k; i++) {
-            temp[i] = nums[n-k+i];
-        }
-
-        // shift the remaining n-k elements to the right
-        for(int i=n-1; i>=k; i--) {
-            nums[i] = nums[i-k];
-        }
-
-        // copy temp back to the beginning
-        for(int i=0; i<k; i++) {
-            nums[i] = temp[i];
-        }
+        reverse(nums.begin(),nums.end());
+        reverse(nums.begin(),nums.begin()+k);
+        reverse(nums.begin()+k,nums.end());
+        
     }
 };
