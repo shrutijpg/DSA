af5cdaa237760c51670c4856e459d9b209042356
shrutijpg <shrutibh1001@gmail.com>
2025-09-03 11:23:00 +0530

    Time: 1 ms (4.61%) | Memory: 15.7 MB (33.13%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     void nextPermutation(vector<int>& nums) {
-        next_permutation(nums.begin(),nums.end());
         
+        next_permutation(nums.begin(),nums.end());     
     }
 };
