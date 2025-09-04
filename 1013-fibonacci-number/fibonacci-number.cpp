c3e25f16c01782b067bcbc2fa3bbeaef204d5b0c
shrutijpg <shrutibh1001@gmail.com>
2025-09-04 11:16:04 +0530

    Time: 13 ms (30.81%) | Memory: 7.8 MB (64.06%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -4,15 +4,9 @@ using namespace std;
 class Solution {
 public:
     // Optimizing vector<int>dp;
-    int func(int i,vector<int>&dp){
-        if(i<=1){
-            return i;
-        }
-        if (dp[i] != -1) return dp[i];
-        return dp[i]=func(i-1,dp)+func(i-2,dp);
-    }
+
     int fib(int n) {
-        vector<int> dp(n+1,-1);
-        return func(n,dp);
+        if(n<=1) return n;
+        return fib(n-1)+fib(n-2);
     }
 };
