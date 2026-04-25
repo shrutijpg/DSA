5cad9b58e2c66d7ffca113cdb1760b061650139c
shrutijpg <shrutibh1001@gmail.com>
2026-04-25 16:55:21 +0530

    Time: 0 ms (100.00%) | Memory: 18.3 MB (30.62%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,16 +1,30 @@
 class Solution {
 private:
-    void dfs(int row,int col,vector<vector<int>>& ans,vector<vector<int>>& image,int color,int iniColor,int delRow[],int delCol[]){
-        ans[row][col] = color;  // new color
-        int n = image.size();
-        int m = image[0].size();
+    void bfs(int row,int col,vector<vector<int>>& ans,vector<vector<int>>& image,int color,int iniColor,int n,int m){
+        ans[row][col] = color; //newcolor
 
-        for(int i=0;i<4;i++){
-            int nr = row+delRow[i];
-            int nc = col + delCol[i];
+        queue<pair<int,int>>q;
+        q.push({row,col});
 
-            if(nr>=0 && nr<n && nc>=0 && nc<m && image[nr][nc] ==iniColor && ans[nr][nc] != color){
-                dfs(nr,nc,ans,image,color,iniColor,delRow,delCol);
+
+        int delRow[] = {-1,0,1,0};
+        int delCol[] = {0,-1,0,1};
+
+
+        while(!q.empty()){
+            int r = q.front().first;
+            int c = q.front().second;
+            q.pop();
+
+            for(int i =0;i<4;i++){
+                int nr = r + delRow[i];
+                int nc = c + delCol[i];
+
+
+                if(nr>=0 && nr<n && nc>=0 && nc<m && image[nr][nc]==iniColor && ans[nr][nc] != color ){
+                    ans[nr][nc] = color;
+                    q.push({nr,nc});
+                }
             }
         }
     }
@@ -18,10 +32,10 @@ public:
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int iniColor = image[sr][sc];
         vector<vector<int>> ans = image;
-        int delRow[] = {-1,0,1,0};
-        int delCol[] = {0,1,0,-1};
-
-        dfs(sr,sc,ans,image,color,iniColor,delRow,delCol);
+        int n = image.size();
+        int m = image[0].size();
+        
+       bfs(sr,sc,ans,image,color,iniColor,n,m);
         return ans;
     }
 };
