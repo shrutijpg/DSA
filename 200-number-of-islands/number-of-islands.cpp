60923e4563d735e418f7b3b97e9e00ac9499f519
shrutijpg <shrutibh1001@gmail.com>
2026-04-25 13:40:59 +0530

    Time: 30 ms (33.41%) | Memory: 24.5 MB (12.91%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,47 +1,59 @@
 class Solution {
-public:
-    void bfs(int row,int col,vector<vector<char>>& grid,vector<vector<int>>&vis){
-        vis[row][col]=1;
+private:
+    void bfs(int row,int col,vector<vector<char>>& grid,vector<vector<int>> &vis){
+        int n = grid.size();
+        int m = grid[0].size();
+
         queue<pair<int,int>> q;
         q.push({row,col});
-        int n = grid.size();
-        int m  = grid[0].size();
+        vis[row][col] = 1;
+
+
+        int delrow[] = {-1,0,1,0};
+        int delcol[] = {0,1,0,-1};
+
+
         while(!q.empty()){
-            int row = q.front().first;
-            int col = q.front().second;
+            int r = q.front().first;
+            int c = q.front().second;
             q.pop();
 
-            //traversing in the neighbours and mark 
-            int drow[] = {-1, 0, 1, 0};
-            int dcol[] = {0, 1, 0, -1};
+            // Exploring neighbours
+            for(int i=0;i<4;i++){
+                int nrow = r+delrow[i];
+                int ncol = c+delcol[i];
 
-            for (int i = 0; i < 4; i++) {
-                int nrow = row + drow[i];
-                int ncol = col + dcol[i];
-
-                if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m &&
-                    grid[nrow][ncol] == '1' && !vis[nrow][ncol]) {
-                    vis[nrow][ncol] = 1;
-                    q.push({nrow, ncol});
+                if(nrow>=0 && nrow<n &&
+                   ncol>=0 && ncol<m &&
+                   grid[nrow][ncol]=='1' && !vis[nrow][ncol]){
+                        vis[nrow][ncol] = 1;
+                        q.push({nrow,ncol});
                 }
-            }
         }
+
+
+        }
+
+        
 
     }
+public:
     int numIslands(vector<vector<char>>& grid) {
         int n = grid.size();
-        int m  = grid[0].size();
-        vector<vector<int>> vis(n,vector<int>(m,0));
-        int cnt=0;
+        int m = grid[0].size();
 
+        vector<vector<int>> vis(n,vector<int>(m,0));
+        int count =0;
+
+        //traversing the grid
         for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(!vis[i][j] && grid[i][j] == '1'){
+            for (int j=0;j<m;j++){
+                if(grid[i][j] == '1' && !vis[i][j]){
+                    count++;
                     bfs(i,j,grid,vis);
-                    cnt++;
                 }
             }
         }
-        return cnt;
+        return count;
     }
 };
